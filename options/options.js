998a1d4ae8f40f8bd09994e194424698f0a2f905
Crowdbotics <team@crowdbotics.com>
2021-08-21 22:42:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_thunder_30013Identifier",
+  url: "https://summer_thunder_30013Identifier.botics.co",
+  api: "https://summer_thunder_30013Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
